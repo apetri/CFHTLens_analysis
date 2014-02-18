24c81f8d08b75b5ef146d088e223a4039b26dfc6
apetri <apetri@phys.columbia.edu>
2014-02-18 17:56:32 -0500

    Reverted 2pt function calculator to serial

--- andrea/catalog_ps.py
+++ andrea/catalog_ps.py
@@ -1,54 +1,26 @@
 import sys
 
-from mpi4py import MPI
-
 import numpy as np
 from scipy.spatial import cKDTree
 
-#Initialize MPI communicator
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
-
 #Prompt user for correct usage
-if(len(sys.argv)<3 and rank==0):
+if(len(sys.argv)<3):
 	print "Usage python %s <catalog_file> <output_file>"%sys.argv[0]
 	exit(1)
 
-#Read total number of galaxies from catalog file
-if(rank==0):
-	print "Determining catalog size..."
-
-x = np.loadtxt(sys.argv[1])[:,0]
-numGal = len(x)
-numGalPerTask = numGal/size
-
-if(rank==0):
-	print "Catalog contains %d galaxies"%numGal
-
-#Decide binning of the 2pt function (theta is the midpoint of each bin)
-step = (x.max()-x.min())/50
-theta = np.arange(step/2,x.max()-x.min()-step/2,step)
-Nbins = len(theta)
-
-if(rank==0):
-	print "Number of bins used: %d"%Nbins
-
 #Load info from catalog: columns (0,1,9,10,11,16,17)=(x,y,w,e1,e2,m,c2)
-if(rank==0):
-	print "Reading catalog..."
-x,y,w,e1,e2,m,c2 = np.loadtxt(sys.argv[1],usecols=[0,1,9,10,11,16,17])[rank*numGalPerTask:(rank+1)*numGalPerTask,:].transpose()
+x,y,w,e1,e2,m,c2 = np.loadtxt(sys.argv[1],usecols=[0,1,9,10,11,16,17]).transpose()
 
 #Build outer products
 e1_ij = np.outer(e1,e1)
 e2_ij = np.outer(e2,e2)
 w_ij = np.outer(w,w)
 
-#Define containers for binned 2pt function
-corrLoc = np.zeros(Nbins)
-corrGlob = np.zeros(Nbins)
-weightLoc = np.zeros(Nbins)
-weightGlob = np.zeros(Nbins)
+#Decide binning of the 2pt function (theta is the midpoint of each bin)
+Nbins = 10
+step = (x.max()-x.min())/Nbins
+theta = np.arange(step/2,x.max()-x.min()-step/2,step)
+corr = np.zeros(Nbins)
 
 #Group x and y into an array to build the KD tree
 X = np.array([x,y]).transpose()
@@ -60,9 +32,7 @@ kdt = cKDTree(X)
 for i in range(Nbins):
 	
 	#Select pairs separated by theta[i] querying the tree
-	if(rank==0):
-		print "Computing correlation bin %d"%(i+1)
-	
+	print "Computing correlation bin %d"%(i+1)
 	pUse = kdt.query_pairs(theta[i]+step/2)
 	pRem = kdt.query_pairs(theta[i]-step/2)
 	pUse.difference_update(pRem)
@@ -70,21 +40,12 @@ for i in range(Nbins):
 	#Vectorize
 	I = np.array(list(pUse)).transpose()
 
-	if(len(I.shape)==2):
-		#Sum over the pairs
-		corrLoc[i] = ((e1_ij[I[0],I[1]] + e2_ij[I[0],I[1]])*w_ij[I[0],I[1]]).sum()
-		weightLoc[i] = w_ij[I[0],I[1]].sum()
+	#Sum over the pairs
+	corr[i] = ((e1_ij[I[0],I[1]] + e2_ij[I[0],I[1]])*w_ij[I[0],I[1]]).sum()
+	weight = w_ij[I[0],I[1]].sum()
+	corr[i] = corr[i]/weight
 
-#Reduce results from all tasks
-if(rank==0):
-	print "Reducing from all tasks..."
+#Output the correlation function
+np.save(sys.argv[2],np.array([theta,corr]))
 
-comm.Barrier()
-comm.Reduce([corrLoc,MPI.DOUBLE],[corrGlob,MPI.DOUBLE],op=MPI.SUM,root=0)
-comm.Reduce([weightLoc,MPI.DOUBLE],[weightGlob,MPI.DOUBLE],op=MPI.SUM,root=0)
-
-#Output results
-comm.Barrier()
-if(rank==0):
-	np.save(sys.argv[2],np.array([theta,corrGlob/weightGlob]))
-	print "Done!!"
+print "Done!"
