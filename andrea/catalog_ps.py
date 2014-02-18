fcfa047acbc84a861ea26c25a3a47fc7cfdbd6ae
apetri <apetri@phys.columbia.edu>
2014-02-18 18:00:55 -0500

    Added verbosity to catalog_ps.py

--- andrea/catalog_ps.py
+++ andrea/catalog_ps.py
@@ -11,6 +11,9 @@ if(len(sys.argv)<3):
 #Load info from catalog: columns (0,1,9,10,11,16,17)=(x,y,w,e1,e2,m,c2)
 x,y,w,e1,e2,m,c2 = np.loadtxt(sys.argv[1],usecols=[0,1,9,10,11,16,17]).transpose()
 
+#Print size of catalog
+print "This catalog contains %d galaxies"%len(x)
+
 #Build outer products
 e1_ij = np.outer(e1,e1)
 e2_ij = np.outer(e2,e2)
@@ -21,6 +24,7 @@ Nbins = 10
 step = (x.max()-x.min())/Nbins
 theta = np.arange(step/2,x.max()-x.min()-step/2,step)
 corr = np.zeros(Nbins)
+weight = np.zeros(Nbins)
 
 #Group x and y into an array to build the KD tree
 X = np.array([x,y]).transpose()
@@ -32,7 +36,7 @@ kdt = cKDTree(X)
 for i in range(Nbins):
 	
 	#Select pairs separated by theta[i] querying the tree
-	print "Computing correlation bin %d"%(i+1)
+	print "Computing correlation bin %d: theta=%.5f"%(i+1,theta[i])
 	pUse = kdt.query_pairs(theta[i]+step/2)
 	pRem = kdt.query_pairs(theta[i]-step/2)
 	pUse.difference_update(pRem)
@@ -42,10 +46,9 @@ for i in range(Nbins):
 
 	#Sum over the pairs
 	corr[i] = ((e1_ij[I[0],I[1]] + e2_ij[I[0],I[1]])*w_ij[I[0],I[1]]).sum()
-	weight = w_ij[I[0],I[1]].sum()
-	corr[i] = corr[i]/weight
+	weight[i] = w_ij[I[0],I[1]].sum()
 
 #Output the correlation function
-np.save(sys.argv[2],np.array([theta,corr]))
+np.save(sys.argv[2],np.array([theta,corr/weight]))
 
 print "Done!"
