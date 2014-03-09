0820da93c5e3c9682565f0b8c4b55685011a5030
liuxx479 <liuxx479@gmail.com>
2014-03-09 14:51:06 -0400

    clean up code, after process 2 fields, 128R, 10min/[field x 4 cosmo x 6 smoothing scale x 128R], so in total I expect 8 hrs for 1000R and 13 fields.

--- jia/massSIM.py
+++ jia/massSIM.py
@@ -5,7 +5,7 @@
 ################ steps #####################
 #1) smoothing, use random galaxy direction, and w as wegith
 #2) KSvw
-#3) count peaks, MF, powerspectrum 
+#3) count peaks, MF, powerspectrum (this will be in a separate code)
 
 import WLanalysis
 from emcee.utils import MPIPool
@@ -23,6 +23,7 @@ ngal_cut = ngal_arcmin*(60**2*12)/512**2# = 0.82, cut = 5 / arcmin^2
 PPR512=8468.416479647716#pixels per radians
 PPA512=2.4633625
 rad2pix=lambda x: around(512/2.0-0.5 + x*PPR512).astype(int) #from radians to pixel location
+sigmaG_arr = (0.5, 1, 1.8, 3.5, 5.3, 8.9)
 
 full_dir = '/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/'
 KS_dir = '/direct/astro+astronfs03/workarea/jia/CFHT/KSsim/'
@@ -32,15 +33,20 @@ hi_w='mQ3-512b240_Om0.260_Ol0.740_w-0.800_ns0.960_si0.800'
 hi_s='mQ3-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.850'
 hi_m='mQ3-512b240_Om0.290_Ol0.710_w-1.000_ns0.960_si0.800'
 
+####### maps to process #########
+i_arr=[1,2]
+R_arr=arange(1,129)
+cosmo_arr=(fidu,hi_m,hi_w,hi_s)
+
+####### end: define constant ####
+
+####### functions ###############
 SIMfn= lambda i, cosmo, R: sim_dir+'%s/raytrace_subfield%i_WL-only_%s_4096xy_%04dr.fit'%(cosmo, i, cosmo, R)
 KSfn = lambda i, cosmo, R, sigmaG, zg: KS_dir+'%s/SIM_KS_sigma%02d_subfield%i_%s_%s_%04dr.fit'%(cosmo, sigmaG*10, i, zg, cosmo,R)#i=subfield, cosmo, R=realization, sigmaG=smoothing, zg=zgroup=(pz, rz, rz2)
 
-sigmaG_arr = (0.5, 1, 1.8, 3.5, 5.3, 8.9)
-
-## generate random rotation while preserve galaxy size and shape info
-
 def rndrot (e1, e2, iseed=None):
-	'''rotate galaxy with ellipticity (e1, e2), by a random angle.
+	'''rotate galaxy with ellipticity (e1, e2), by a random angle. 
+	generate random rotation while preserve galaxy size and shape info
 	'''
 	if iseed:
 		random.seed(iseed)
@@ -53,10 +59,17 @@ def rndrot (e1, e2, iseed=None):
 # b=rndrot(a)
 # abs(a)==abs(b) #return True
 
-
 zcut_idx = lambda i: WLanalysis.readFits(full_dir+'zcut_idx_subfield%i.fit'%(i))
 
 def eobs_fun (g1, g2, k, e1, e2):
+	'''van wearbeke 2013 eq 5-6, get unbiased estimator for shear.
+	Input:
+	g1, g2: shear
+	k: convergence
+	e1, e2: galaxy intrinsic ellipticity
+	Output:
+	e_obs1, e_obs2
+	'''
 	g = (g1+1j*g2)/(1-k)
 	eint = e1+1j*e2
 	eobs = (g+eint)/(1-g*eint)
@@ -74,12 +87,12 @@ def fileGen(i, R, cosmo):
 	Mw = w
 	'''
 	### these are from simulation
-	Ms1_pz_fn  = KS_dir+'%s/SIM_Ms1_pz_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
-	Ms2_pz_fn  = KS_dir+'%s/SIM_Ms2_pz_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
-	Ms1_rz1_fn = KS_dir+'%s/SIM_Ms1_rz1_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
-	Ms2_rz1_fn = KS_dir+'%s/SIM_Ms2_rz1_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
-	Ms1_rz2_fn = KS_dir+'%s/SIM_Ms1_rz2_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
-	Ms2_rz2_fn = KS_dir+'%s/SIM_Ms2_rz2_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
+	Ms1_pz_fn  = KS_dir+'SIM_Ms/SIM_Ms1_pz_subfield%i_%s_%04dr.fit' %(i, cosmo, R)
+	Ms2_pz_fn  = KS_dir+'SIM_Ms/SIM_Ms2_pz_subfield%i_%s_%04dr.fit' %(i, cosmo, R)
+	Ms1_rz1_fn = KS_dir+'SIM_Ms/SIM_Ms1_rz1_subfield%i_%s_%04dr.fit'%(i, cosmo, R)
+	Ms2_rz1_fn = KS_dir+'SIM_Ms/SIM_Ms2_rz1_subfield%i_%s_%04dr.fit'%(i, cosmo, R)
+	Ms1_rz2_fn = KS_dir+'SIM_Ms/SIM_Ms1_rz2_subfield%i_%s_%04dr.fit'%(i, cosmo, R)
+	Ms2_rz2_fn = KS_dir+'SIM_Ms/SIM_Ms2_rz2_subfield%i_%s_%04dr.fit'%(i, cosmo, R)
 	Mw_fn = KS_dir+'SIM_Mw_subfield%i.fit'%(i) # same for all R
 
 	Marr = (Mw_fn, Ms1_pz_fn, Ms2_pz_fn, Ms1_rz1_fn, Ms2_rz1_fn, Ms1_rz2_fn, Ms2_rz2_fn)
@@ -102,6 +115,9 @@ def fileGen(i, R, cosmo):
 		createfiles = 2 #flag to create everything
 
 	if createfiles:
+		idx = zcut_idx (i)
+		y, x, e1, e2, w = (WLanalysis.readFits(full_dir+'yxew_zcut0213_subfield%i.fit'%(i))).T
+	
 		#idx = zcut_idx (i)#redshift cut
 		#simfile = WLanalysis.readFits(SIMfn(i,cosmo,R))[idx, [0,1,2,4,5,6,8,9,10]]#simulation file at redshift cut
 		#s1_pz, s2_pz, k_pz, s1_rz1, s2_rz1, k_rz1, s1_rz2, s2_rz2, k_rz2 = simfile.T
@@ -126,7 +142,7 @@ def fileGen(i, R, cosmo):
 			WLanalysis.writeFits(iM, Marr[j])
 			j+=1
 		### add Mk just for comparison ###
-		Mk_fn = KS_dir+'%s/SIM_Mk_rz1_subfield%i_%s_%04dr.fit'%(cosmo, i, cosmo, R)
+		Mk_fn = KS_dir+'SIM_Mk/SIM_Mk_rz1_subfield%i_%s_%04dr.fit'%(i, cosmo, R)
 		WLanalysis.writeFits(Mk, Mk_fn)
 		
 	return Ms1_pz, Ms2_pz, Ms1_rz1, Ms2_rz1, Ms1_rz2, Ms2_rz2, Mw
@@ -163,26 +179,28 @@ def KSmap(iiRcosmo):
 				WLanalysis.writeFits(kmap, KS_fn)
 
 # test KSmap(1, 1, fidu), pass
-
-for i in (1,2):
+def iRcosmo_fcn(i_arr, R_arr, cosmo_arr):
+	'''Prepare list for mapping KSmap function.
+	'''
+	iRcosmo=[[1,1,''],]*(len(i_arr)*len(R_arr)*len(cosmo_arr))
 	j=0
-	iRcosmo=[[1,1,''],]*(128*4)
-	for R in arange(1,129):
-		for cosmo in (fidu,hi_m,hi_w,hi_s):
-			iRcosmo[j]=[i,R,cosmo]
-			j+=1
+	for i in i_arr:
+		for R in R_arr:
+			for cosmo in cosmo_arr:
+				iRcosmo[j]=[i,R,cosmo]
+				j+=1
+	return iRcosmo
 
-	idx = zcut_idx (i)
-	y, x, e1, e2, w = (WLanalysis.readFits(full_dir+'yxew_zcut0213_subfield%i.fit'%(i))).T
 
-	## Initialize the MPI pool
-	pool = MPIPool()
-	## Make sure the thread we're running on is the master
-	#if not pool.is_master():
-		#pool.wait()
-		#sys.exit(0)
-	## logger.debug("Running with MPI...")
-	pool.map(KSmap, iRcosmo)
+iRcosmo = iRcosmo_fcn(i)
 
+## Initialize the MPI pool
+pool = MPIPool()
+## Make sure the thread we're running on is the master
+#if not pool.is_master():
+	#pool.wait()
+	#sys.exit(0)
+## logger.debug("Running with MPI...")
+pool.map(KSmap, iRcosmo)
 	
-savetxt(KS_dir+'done.ls',zeros(5))
+savetxt(KS_dir+'done.ls','done')
