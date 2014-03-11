129e25ede1653062589a48b023cc82b1c2c6907f
liuxx479 <liuxx479@gmail.com>
2014-03-11 15:21:30 -0400

    cosmo model cov

--- jia/CosmoAnalysis.py
+++ jia/CosmoAnalysis.py
@@ -16,12 +16,12 @@ from multiprocessing import Pool # has bug on my laptop, but fine with astro
 
 ################ steps #####################
 #1) count peaks, with mask
-#2) power spectrum, with mask, but note need to use good fields (to be implemented)
+#2) power spectrum, without mask, but note need to use good fields (to be implemented)
 #3) construct model, using rz1
 #4) fit model to CFHT
 #5) fit model to simulation pz, rz2
 
-########## define constants ############
+########## beging: define constants ############
 kmin = -0.04 # lower bound of kappa bin = -2 SNR
 kmax = 0.12 # higher bound of kappa bin = 6 SNR
 ngal_arcmin = 5.0
@@ -42,15 +42,23 @@ hi_s='mQ3-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.850'
 hi_m='mQ3-512b240_Om0.290_Ol0.710_w-1.000_ns0.960_si0.800'
 
 ####### maps to process #########
+processmaps = 0
+
 bins_arr = arange(10, 110, 15)
 sigmaG_arr = (0.5, 1, 1.8, 3.5, 5.3, 8.9)
 i_arr=[1,2]
 R_arr=arange(1,129)
 cosmo_arr=(fidu,hi_m,hi_w,hi_s)
 Rtol=len(R_arr)
+
+####### cosmology model configuration ######
+
+config_2_21 = array([[1.8, 25, 3, 17],
+		     [3.5, 25, 5, 12]])#2 smoothing scale, tot 24 bins
+config = config_2_21
 ####### end: define constant ####
 
-########## functions ###########
+########## beging: functions ###########
 KSCFHT_fn = lambda i, sigmaG: KSCFHT_dir+'CFHT_KS_sigma%02d_subfield%02d.fits'%(sigmaG*10, i)
 
 Mask_fn = lambda i, sigmaG: KSCFHT_dir+'CFHT_mask_ngal5_sigma%02d_subfield%02d.fits'%(sigmaG*10, i)
@@ -101,20 +109,48 @@ def Pmat (iRcosmo, Rtol=Rtol, R0 = 1):
 		mat = array(map(map_fcn,R_arr))
 		WLanalysis.writeFits(mat, fn)
 	return mat
+############ end: functions ##############
 
+############ begein: calculate ############
 
+#1. process maps
+if processmaps:
 ## Make sure the thread we're running on is the master
 #if not pool.is_master():
 	#pool.wait()
 	#sys.exit(0)
-## logger.debug("Running with MPI...")
-iRcosmo_pk = [[i, sigmaG, zg, bins, cosmo] for i in i_arr for sigmaG in sigmaG_arr for zg in zg_arr for bins in bins_arr for cosmo in cosmo_arr]
+	## logger.debug("Running with MPI...")
+	iRcosmo_pk = [[i, sigmaG, zg, bins, cosmo] for i in i_arr for sigmaG in sigmaG_arr for zg in zg_arr for bins in bins_arr for cosmo in cosmo_arr]
+	iRcosmo_ps = [[i, 0.5, zg, 0, cosmo] for i in i_arr for zg in zg_arr for cosmo in cosmo_arr]
+	pool = MPIPool()
+	pool.map(Pmat, iRcosmo_ps+iRcosmo_pk)
+	pool.close()
 
-iRcosmo_ps = [[i, 0.5, zg, 0, cosmo] for i in i_arr for zg in zg_arr for cosmo in cosmo_arr]
+#2. cosmology model
+#2.1 covariance matrix
+# (1.8 arcmin, 25bins)[3:17] # 14 bins
+# (3.5 arcmin, 25bins)[5:12] # 7 bins
+## build array of sigmaG, bins, start, end, to prepare for cov, fisher mat
+#config_2_21 = array([[1.8, 25, 3, 17],
+		     #[3.5, 25, 5, 12]])
+bintol = int(sum(config[:,-1]-config[:,-2])) # total bins used in cosmo model
+cosmo_mat = zeros((4, Rtol, bintol))
+j = 0
+for cosmo in cosmo_arr:
+	for i in i_arr:
+		k = 0
+		for iconfig in config:
+			sigmaG, bins, x0, x1 = iconfig
+			l = x1-x0
+			imat = Pmat((i, sigmaG, 'rz1', bins, cosmo))[:,x0:x1]
+			cosmo_mat[j, :, k:k+l] += imat
+			k += x1-x0
+	j+=1
 
-pool = MPIPool()
-pool.map(Pmat, iRcosmo_ps+iRcosmo_pk)
+cov_mat = cov(cosmo_mat[0],rowvar=0)#rowvar is the row contaning observations, aka 128R
+
+
+############# end: calculate ###############
 savetxt(KSsim_dir+'done_ps.ls','done')
 print 'done-done-done!'
-pool.close()
 sys.exit(0)
