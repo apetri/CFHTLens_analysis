a5dbc82b3002599fc4470b872e9cf5ec19fe02cd
liuxx479 <liuxx479@gmail.com>
2014-03-10 23:05:50 -0400

    fix bug

--- jia/CosmoAnalysis.py
+++ jia/CosmoAnalysis.py
@@ -98,8 +98,8 @@ def Pmat (iRcosmo, Rtol=Rtol, R0 = 1):
 		print 'i, bins, sigmaG', i, bins, sigmaG
 		map_fcn = Psingle (i, sigmaG, zg, bins, cosmo, ps=bins)
 		#p = Pool(Rtol/4)#use multiprocessing on 1 single core
-		ipeaks_mat = array(map(map_fcn,R_arr))
-		WLanalysis.writeFits(ipeaks_mat, fn)
+		mat = array(map(map_fcn,R_arr))
+		WLanalysis.writeFits(mat, fn)
 	return mat
 
 
