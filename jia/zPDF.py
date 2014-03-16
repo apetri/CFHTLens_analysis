70d733e0f8bf3a19c77c0f720905dc6b39907e03
liuxx479 <liuxx479@gmail.com>
2014-03-16 17:40:56 +0100

    fix bug

--- jia/zPDF.py
+++ jia/zPDF.py
@@ -17,21 +17,22 @@ SIMbins = edges[:-1]+binwidth/2
 w = sum(binwidth)/binwidth/70
 
 def pdfs(i):#subfield count
-	m=genfromtxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/full_subfield%s'%(i),usecols=range(5)) 
+	idx=readFits('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/zcut_idx_subfield%i.fit'%(i)) 
+	m=genfromtxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/full_subfield%s'%(i),usecols=range(5)) [idx]
 	pk=m[:,2]
 	rndz=m[:,3:5].flatten()
 	pk=histogram(pk,bins=edges)[0]/float(len(pk))
 	rndz=histogram(rndz,bins=edges)[0]/float(len(rndz))
-	a=genfromtxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/full_subfield%s'%(i),usecols=range(89-70,89)) 
-	a/=sum(a,axis=1)[:,newaxis]
-	aa=average(a,axis=0) 
-	savetxt('aa_arr%s'%(i),aa)
-	savetxt('pks_arr%s'%(i),pk)
-	savetxt('rnds_arr%s'%(i),rndz)
+	all_PDF=genfromtxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/full_subfield%s'%(i),usecols=range(89-70,89)) [idx]
+	all_PDF/=sum(all_PDF,axis=1)[:,newaxis]
+	average_PDF=average(all_PDF,axis=0) 
+	#savetxt('avgPDF_arr%s'%(i),average_PDF)
+	#savetxt('pks_arr%s'%(i),pk)
+	#savetxt('rnds_arr%s'%(i),rndz)
 	# for CFHT each bin has 3.5/70 probability, for SIM, each bin has 3.5/67 height
-	return aa, pk, rndz, len(a)
+	return all_PDF, pk, rndz, len(all_PDF)
 
-aa_arr = zeros(shape=(13,70))
+avgPDF_arr = zeros(shape=(13,70))
 pks_arr = zeros(shape=(13,67))
 rnds_arr =  zeros(shape=(13,67))
 lena_arr = zeros(13)
@@ -42,13 +43,13 @@ x=p.map(pdfs,range(1,14))
 
 for i in range(1,14):
 	aa, peaks, rnds, lena=x[i-1]
-	aa_arr[i-1]=aa
+	avgPDF_arr_arr[i-1]=aa
 	pks_arr[i-1]=peaks
 	rnds_arr[i-1]=rnds
 	lena_arr[i-1]=lena
 	print i, lena
 
-savetxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/aa_arr.ls',aa_arr)
+savetxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/avgPDF_arr.ls',avgPDF_arr)
 savetxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/pks_arr.ls',pks_arr)
 savetxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/rnds_arr.ls',rnds_arr)
 savetxt('/direct/astro+astronfs01/workarea/jia/CFHT/full_subfields/lena_arr.ls',lena_arr)
