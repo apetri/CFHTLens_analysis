c54b978a948ace91d7cf93c64fd70ceb3b660342
liuxx479 <liuxx479@gmail.com>
2014-05-14 18:35:01 -0400

    fix bug

--- jia/create_galn.py
+++ jia/create_galn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import *
 import WLanalysis
-from multiprocessing import Pool
+from emcee.utils import MPIPool
 
 sigmaG = 0.5
 emucat_dir = '/direct/astro+astronfs03/workarea/jia/CFHT/CFHT/raytrace_subfields/emulator_galpos_zcut0213/'
@@ -33,8 +33,12 @@ def ps (R):
 for i in range(1, 14):
 	for cosmo in cosmo_arr:
 		print i, cosmo
-		p = Pool(1000)
+		p = MPIPool()
 		pmat = np.array(p.map(ps, arange(1,1001)))
 		pmat_fn = KS_dir + 'powspec_Mk/SIM_powspec_sigma05_subfield%i_rz1_%s_1000R.fit'%(i,cosmo)
-		WLanalysis.writeFits(pmat,pmat_fn)
+		p.close()
+		try:
+			WLanalysis.writeFits(pmat,pmat_fn)
+		except Exception:
+			pass
 print 'done'
