47297e28b9a5fa36bc4176c8ef51b6a987b28978
liuxx479 <liuxx479@gmail.com>
2014-07-16 17:54:05 -0400

    Coords2grid OrganizeSplitFile processing stampede

--- jia/coords2grid_Wx_stampede.py
+++ jia/coords2grid_Wx_stampede.py
@@ -12,11 +12,13 @@ import numpy as np
 from scipy import *
 from pylab import *
 import os
-#import astropy.io.fits as pyfits
 import WLanalysis
+from emcee.utils import MPIPool
 
-split_dir = '/home1/02977/jialiu/CFHT_cat/split/'
-W_dir = lambda Wx: '/home1/02977/jialiu/CFHT_cat/%s/'%(Wx) #dir for W1..W4 field
+cat_dir='/home1/02977/jialiu/CFHT_cat/'
+#cat_dir = '/Users/jia/CFHTLenS/catalogue/'
+split_dir = cat_dir+'split/'
+W_dir = lambda Wx: cat_dir+'W%s/'%(Wx) #dir for W1..W4 field
 splitfiles = os.listdir(split_dir)
 RA1 =(30.0, 39.0)#starting RA for W1
 DEC1=(-11.5,-3.5)
@@ -28,6 +30,7 @@ RA4 =(329.5, 336.0)
 DEC4=(-1.2, 5.0)
 RAs=(RA1,RA2,RA3,RA4)
 DECs=(DEC1,DEC2,DEC3,DEC4)
+centers = array([[34.5, -7.5], [134.5, -3.25],[214.5, 54.5],[ 332.75, 1.9]])
 
 dpp=0.0016914558667664816#degrees per pixel = sqrt(12)/2048
 xnum = lambda RA: round((amax(RA)-amin(RA))/dpp+1)
@@ -40,80 +43,53 @@ field2int = lambda str: int(str[1])
 #DrawRedshifts = lambda iPz: concatenate([[z_arr[argmax(iPz)],], WLanalysis.DrawFromPDF(z_arr, iPz, 2)])
 DrawRedshifts = lambda iPz: WLanalysis.DrawFromPDF(z_arr, iPz, 1)
 
-def list2coords(radeclist): 
+def list2coords(radeclist, Wx): 
 	'''Input: radeclist = (Wfield, ra, dec), a 3xN matrix, (ra, dec) in degrees
 	Return: (subfield, x, y), a 3xN matrix, (x, y) in radians
 	'''
-	xylist = zeros(shape = radeclist.shape)
-	j = 0 #subfield count
-	for i in range(4): #W field count
-		idx = where(radeclist[:,0] == i+1)[0] #find W1 enries
-		if len(idx) > 0:
-			print 'Found entry for W',i+1
-			sublist = radeclist[idx] #pick out W1 entries
-			print 'idx',idx
-			sort_subf = sort_subfs[i] #get W1 configurations
-			center = centers[i] #prepare for x,y calc
-			f_Wx = gnom_fun(center)
-			xy = degrees(array(map(f_Wx,sublist[:,1:3])))
-			print 'xy',xy
-			for isort_subf in sort_subf:
-				x0,x1,y0,y1 = isort_subf[5:9]
-				# find entries for each subfield
-				iidx = where((xy[:,0]<x1)&(xy[:,0]>x0)&(xy[:,1]<y1)&(xy[:,1]>y0))[0]
-				
-				if len(iidx) > 0:
-					print 'iidx j=',j,iidx
-					isublist = sublist[iidx]#subfield
-					icenter = isort_subf[-2:]#center for subfield
-					f_sub = gnom_fun(icenter)
-					xy_sub = array(map(f_sub,isublist[:,1:3]))
-					xylist[idx[iidx],0] = j2s(j)+1
-					xylist[idx[iidx],1:] = xy_sub					
-					if j in (4,9): # needs to turn 90 degrees, counterclock
-						iy = degrees(xy_sub.T[0])
-						ix =- degrees(xy_sub.T[1])
-					else:
-						ix = degrees(xy_sub.T[0])
-						iy = degrees(xy_sub.T[1])
-				j+=1
-		else:
-			j+=len(sort_subfs[i])
-	return xylist
+	xy = zeros(shape = radeclist.shape) #create xy list
+	center = centers[Wx-1] #find the center for Wx field
+	f_Wx = WLanalysis.gnom_fun(center)
+	xy = degrees(array(map(f_Wx,radeclist)))
+	return xy
 
-def OrganizeSplitFile(ifile):#file2coord(ifile):
+def OrganizeSplitFile(ifile):
 	'''read in one of the split file, pick out the redshift, and sort by fields, e2 is c2 correted, with e2-=c2'''	
 	
 	field = genfromtxt(split_dir+ifile,usecols=0,dtype=str)
 	field = array(map(field2int,field))
-	print 'field',field.shape,field
+	print ifile
 
 	# generate 2 random redshift and 1 peak
 	Pz = genfromtxt(split_dir+ifile,usecols=arange(14,84),dtype=str)
 	Pz = (np.core.defchararray.replace(Pz,',','')).astype(float)
 	
 	seed(99)
-	z_rand1 = array(map(DrawRedshifts,Pz))
+	z_rand1 = array(map(DrawRedshifts,Pz)).ravel()
 	seed(88)
-	z_rand2 = array(map(DrawRedshifts,Pz))
+	z_rand2 = array(map(DrawRedshifts,Pz)).ravel()
 	z_peak = z_arr[argmax(Pz,axis=1)]
-	#Prand = concatenate([z_peak.reshape(-1,1), z_rand],axis=1)
-	#print 'Prand',Prand.shape,datas
+	z_all = concatenate([[z_peak,], [z_rand1,], [z_rand2,]]).T
 
 	sheardata = genfromtxt(split_dir+ifile,usecols=[1,2,5,6,7,8,9,10,11,12,13,84])
-	##skip_header=1
-	ra, dec, e1, e2, w, fitclass, r, snr, mask, m, c2, mag= sheardata.T
+	ra, dec, e1, e2, w, fitclass, r, snr, mask, m, c2, mag = sheardata.T
 	e2 -= c2
 	
-	radeclist = concatenate((field.reshape(-1,1),sheardata[[0,1]]),axis=1)
-	xylist = list2coords(radeclist)
-		
+	
 	i=0
 	for Wx in range(1,5):
-		idx=where((field==Wx)&(mask<=1.0)&(fitclass==0)&(z>=0.2)&(z<=1.3))
-		print ifile, Wx, len(idx[0])/50000.0
-		if len(idx[0]) > 0:
+		idx=where((field==Wx)&(mask<=1.0)&(fitclass==0)&(amin(z_all,axis=-1)>=0.2)&(amax(z_all,axis=-1)<=1.3))[0]
+		print ifile, Wx, len(idx)/50000.0
+		if len(idx) > 0:
 			#data = (np.array([ra,dec,e1,e2,w,r,snr,m,c2,mag]).T)[idx]
-			data = (np.array([ra,dec,e1,e2,w,r,snr,m,c2,mag,z]).T)[idx]
-			WLanalysis.writeFits(data, W_dir(Wx)+ifile+'.fit')#,fmt=['%i','%i','%s','%s','%s','%.3f'])
+			data = (np.array([ra,dec,e1,e2,w,r,snr,m,c2,mag,z_peak, z_rand1, z_rand2]).T)[idx]
+			radeclist = sheardata[idx][:,[0,1]]
+			xylist = list2coords(radeclist, Wx)
+			xy_data = concatenate([xylist,data],axis=1)
+			WLanalysis.writeFits(xy_data, W_dir(Wx)+ifile+'.fit')#,fmt=['%i','%i','%s','%s','%s','%.3f'])
 		i+=1
+
+#ifile = splitfiles[0]
+#OrganizeSplitFile(ifile)
+pool = MPIPool()
+pool.map(OrganizeSplitFile,splitfiles)
