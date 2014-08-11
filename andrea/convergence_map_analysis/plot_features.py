ecfe0677cdc73f4ca8852ee251d4ec4a3b373a1e
apetri <apetri@phys.columbia.edu>
2014-08-11 17:30:21 -0400

    plot summary between subfields

--- andrea/convergence_map_analysis/plot_features.py
+++ andrea/convergence_map_analysis/plot_features.py
@@ -78,7 +78,7 @@ if __name__=="__main__":
 		fig_minkowski,ax_minkowski = plt.subplots(1,3,figsize=(24,8))
 
 		#Plot for the sumulations 
-		for model in training_models:
+		for model in training_models + [observed_model]:
 
 			m = Measurement(model=model,options=options,subfield=subfield,smoothing_scale=smoothing_scale,measurer=None)
 			m.get_all_map_names()
@@ -89,13 +89,21 @@ if __name__=="__main__":
 			ensemble_power = Ensemble.fromfilelist([os.path.join(m.full_save_path,"power_spectrum.npy")])
 			ensemble_power.load(from_old=True)
 			P = ensemble_power.mean()
-			ax_power.plot(l,l*(l+1)*P/(2*np.pi))
+
+			if type(model) == CFHTLens:
+				ax_power.plot(l,l*(l+1)*P/(2*np.pi),linestyle="--",color="black")		
+			elif type(model) == CFHTemu1:
+				ax_power.plot(l,l*(l+1)*P/(2*np.pi))
 
 			#Peaks
 			ensemble_peaks = Ensemble.fromfilelist([os.path.join(m.full_save_path,"peaks.npy")])
 			ensemble_peaks.load(from_old=True)
 			pk = ensemble_peaks.mean()
-			ax_peaks.plot(v_pk,pk)
+			
+			if type(model) == CFHTLens:
+				ax_peaks.plot(v_pk,pk,linestyle="--",color="black")
+			elif type(model) ==  CFHTemu1:
+				ax_peaks.plot(v_pk,pk)
 
 			#Minkowski functionals
 			ensemble_minkowski = Ensemble.fromfilelist([os.path.join(m.full_save_path,"minkowski_all.npy")])
@@ -106,40 +114,97 @@ if __name__=="__main__":
 			mf1 = mf_all[num_mf_bins:2*num_mf_bins]
 			mf2 = mf_all[2*num_mf_bins:]
 			
-			ax_minkowski[0].plot(v_mf,mf0)
-			ax_minkowski[1].plot(v_mf,mf1)
-			ax_minkowski[2].plot(v_mf,mf2)
-
-		#Plot the observations
-		m = Measurement(model=observed_model,options=options,subfield=subfield,smoothing_scale=smoothing_scale,measurer=None)
-		m.get_all_map_names()
-
-		#Load the features and plot
-
-		#Power spectrum
-		ensemble_power = Ensemble.fromfilelist([os.path.join(m.full_save_path,"power_spectrum.npy")])
-		ensemble_power.load(from_old=True)
-		P = ensemble_power.mean()
-		ax_power.plot(l,l*(l+1)*P/(2*np.pi),linestyle="--",color="black")
-
-		#Peaks
-		ensemble_peaks = Ensemble.fromfilelist([os.path.join(m.full_save_path,"peaks.npy")])
-		ensemble_peaks.load(from_old=True)
-		pk = ensemble_peaks.mean()
-		ax_peaks.plot(v_pk,pk,linestyle="--",color="black")
-
-		#Minkowski functionals
-		ensemble_minkowski = Ensemble.fromfilelist([os.path.join(m.full_save_path,"minkowski_all.npy")])
-		ensemble_minkowski.load(from_old=True)
+			if type(model) == CFHTLens:
+				ax_minkowski[0].plot(v_mf,mf0,linestyle="--",color="black")
+				ax_minkowski[1].plot(v_mf,mf1,linestyle="--",color="black")
+				ax_minkowski[2].plot(v_mf,mf2,linestyle="--",color="black")	
+			elif type(model) == CFHTemu1:
+				ax_minkowski[0].plot(v_mf,mf0)
+				ax_minkowski[1].plot(v_mf,mf1)
+				ax_minkowski[2].plot(v_mf,mf2)
 		
-		mf_all = ensemble_minkowski.mean()
-		mf0 = mf_all[:num_mf_bins]
-		mf1 = mf_all[num_mf_bins:2*num_mf_bins]
-		mf2 = mf_all[2*num_mf_bins:]
+
+		fig_minkowski.tight_layout()
+
+		#Save the figures
+		fig_power.savefig(os.path.join(save_path,"plots","power_subfield{0}_sigma{1:02d}.png".format(subfield,int(smoothing_scale*10))))
+		fig_peaks.savefig(os.path.join(save_path,"plots","peaks_subfield{0}_sigma{1:02d}.png".format(subfield,int(smoothing_scale*10))))
+		fig_minkowski.savefig(os.path.join(save_path,"plots","minkowski_subfield{0}_sigma{1:02d}.png".format(subfield,int(smoothing_scale*10))))
+
+
+	##########################################################################################
+	#######If there is more than one subfield plot also average between subfields#############
+	##########################################################################################
+
+	if len(subfields)>1:
 		
-		ax_minkowski[0].plot(v_mf,mf0,linestyle="--",color="black")
-		ax_minkowski[1].plot(v_mf,mf1,linestyle="--",color="black")
-		ax_minkowski[2].plot(v_mf,mf2,linestyle="--",color="black")
+		fig_power,ax_power = plt.subplots()
+		fig_peaks,ax_peaks = plt.subplots()
+		fig_minkowski,ax_minkowski = plt.subplots(1,3,figsize=(24,8))
+
+		for model in training_models + [observed_model]:
+
+			ensemble_power_all = Ensemble()
+			ensemble_peaks_all = Ensemble()
+			ensemble_minkowski_all = Ensemble()
+
+			#Accumulate subfields
+			for subfield in subfields:
+
+				m = Measurement(model=model,options=options,subfield=subfield,smoothing_scale=smoothing_scale,measurer=None)
+				m.get_all_map_names()
+
+				#Load the features and plot
+
+				#Power spectrum
+				ensemble_power = Ensemble.fromfilelist([os.path.join(m.full_save_path,"power_spectrum.npy")])
+				ensemble_power.load(from_old=True)
+				ensemble_power_all += ensemble_power
+
+				#Peaks
+				ensemble_peaks = Ensemble.fromfilelist([os.path.join(m.full_save_path,"peaks.npy")])
+				ensemble_peaks.load(from_old=True)
+				ensemble_peaks_all += ensemble_peaks
+
+				#Minkowski functionals
+				ensemble_minkowski = Ensemble.fromfilelist([os.path.join(m.full_save_path,"minkowski_all.npy")])
+				ensemble_minkowski.load(from_old=True)
+				ensemble_minkowski_all += ensemble_minkowski
+
+			#Average and plot
+			if type(model)==CFHTLens:
+				
+				P = ensemble_power_all.mean()
+				ax_power.plot(l,l*(l+1)*P/(2*np.pi),linestyle="--",color="black")
+
+				pk = ensemble_peaks_all.mean()
+				ax_peaks.plot(v_pk,pk,linestyle="--",color="black")
+
+				mf_all = ensemble_minkowski_all.mean()
+				mf0 = mf_all[:num_mf_bins]
+				mf1 = mf_all[num_mf_bins:2*num_mf_bins]
+				mf2 = mf_all[2*num_mf_bins:]
+			
+				ax_minkowski[0].plot(v_mf,mf0,linestyle="--",color="black")
+				ax_minkowski[1].plot(v_mf,mf1,linestyle="--",color="black")
+				ax_minkowski[2].plot(v_mf,mf2,linestyle="--",color="black")
+
+			elif type(model)==CFHTemu1:
+
+				P = ensemble_power_all.mean()
+				ax_power.plot(l,l*(l+1)*P/(2*np.pi))
+
+				pk = ensemble_peaks_all.mean()
+				ax_peaks.plot(v_pk,pk)
+
+				mf_all = ensemble_minkowski_all.mean()
+				mf0 = mf_all[:num_mf_bins]
+				mf1 = mf_all[num_mf_bins:2*num_mf_bins]
+				mf2 = mf_all[2*num_mf_bins:]
+			
+				ax_minkowski[0].plot(v_mf,mf0)
+				ax_minkowski[1].plot(v_mf,mf1)
+				ax_minkowski[2].plot(v_mf,mf2)
 
 		#Set the axes labels
 		ax_power.set_xlabel(r"$l$")
@@ -159,6 +224,7 @@ if __name__=="__main__":
 		fig_minkowski.tight_layout()
 
 		#Save the figures
-		fig_power.savefig(os.path.join(save_path,"plots","power_subfield{0}_sigma{1:02d}.png".format(subfield,int(smoothing_scale*10))))
-		fig_peaks.savefig(os.path.join(save_path,"plots","peaks_subfield{0}_sigma{1:02d}.png".format(subfield,int(smoothing_scale*10))))
-		fig_minkowski.savefig(os.path.join(save_path,"plots","minkowski_subfield{0}_sigma{1:02d}.png".format(subfield,int(smoothing_scale*10))))
+		fig_power.savefig(os.path.join(save_path,"plots","power_all_subfields_sigma{1:02d}.png".format(int(smoothing_scale*10))))
+		fig_peaks.savefig(os.path.join(save_path,"plots","peaks_all_subfields_sigma{1:02d}.png".format(int(smoothing_scale*10))))
+		fig_minkowski.savefig(os.path.join(save_path,"plots","minkowski_all_subfields_sigma{1:02d}.png".format(int(smoothing_scale*10))))
+
