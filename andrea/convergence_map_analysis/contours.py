71d11799123c2844864a875a60a17c6508f699a1
apetri <apetri@phys.columbia.edu>
2014-08-12 11:48:31 -0400

    prettify contour plotting

--- andrea/convergence_map_analysis/contours.py
+++ andrea/convergence_map_analysis/contours.py
@@ -77,15 +77,24 @@ def likelihood_values(likelihood,levels=[0.684],epsilon=0.01,max_iterations=1000
 ##############Plot the contours on top of the likelihood##############
 ######################################################################
 
-def plot_contours(ax,likelihood,values,**kwargs):
+def plot_contours(ax,likelihood,values,levels,display_percentages,**kwargs):
 
 	assert "colors" in kwargs.keys() and "extent" in kwargs.keys()
 	assert len(kwargs["colors"]) == len(values)
 
 	ax1 = ax.imshow(likelihood,origin="lower",cmap=plt.cm.binary_r,extent=kwargs["extent"],aspect="auto")
 	plt.colorbar(ax1,ax=ax)
+
+	#Build contour levels
+	fmt = dict()
 	
-	ax.contour(likelihood,values,colors=kwargs["colors"],origin="lower",extent=kwargs["extent"],aspect="auto")
+	for n,value in enumerate(values):
+		fmt[value] = "{0:.1f}%".format(levels[n]*100)
+
+	cs = ax.contour(likelihood,values,colors=kwargs["colors"],origin="lower",extent=kwargs["extent"],aspect="auto")
+	
+	if display_percentages:
+		plt.clabel(cs,fmt=fmt,inline=1,fontsize=9)
 
 ################################################################
 #####################Main execution#############################
@@ -93,14 +102,56 @@ def plot_contours(ax,likelihood,values,**kwargs):
 
 if __name__=="__main__":
 
-	if len(sys.argv < 2):
-		print("Usage {0} <3D-numpy-likelihood>".format(sys.argv[0]))
+	#Parameters of which we want to compute the confidence estimates
+	cosmo_parameters = ["Omega_m","w","sigma8"]
+	cosmo_labels = {"Omega_m":r"$\Omega_m$","w":r"$w$","sigma8":r"$\sigma_8$"}
+	
+	#Parse command line options
+	parser = argparse.ArgumentParser(prog=sys.argv[0])
+	parser.add_argument("likelihood_npy_file",nargs="+")
+	parser.add_argument("-f","--file",dest="options_file",action="store",type=str,help="analysis options file")
+
+	cmd_args = parser.parse_args()
+
+	if cmd_args.options_file is None:
+		parser.print_help()
 		sys.exit(0)
 
-	full_likelihood = np.load(sys.argv[1])
+	full_likelihood = np.load(cmd_args.likelihood_npy_file[0])
 
-	#Marginalize over w
-	marginalized_likelihood = full_likelihood.sum(1)
+	#Parse options from configuration file
+	options = ConfigParser.ConfigParser()
+	with open(cmd_args.options_file,"r") as configfile:
+		options.readfp(configfile)
+
+	#Decide the axis on which to marginalize
+	marginalize_over = options.get("contours","marginalize_over")
+	if marginalize_over == "Omega_m":
+		marginalize_axis = 0
+	elif marginalize_over == "w":
+		marginalize_axis = 1
+	elif marginalize_over == "sigma8":
+		marginalize_axis = 2
+	else:
+		raise ValueError("Invalid parameter name")
+
+	#Decide the confidence levels to display
+	levels = [ float(level) for level in options.get("contours","levels").split(",") ]
+	#Parse a list of pretty colors
+	colors = options.get("contours","colors").split(",")
+
+	#Set the extent of the plot once the parameters to display are known
+	cosmo_parameters.pop(cosmo_parameters.index(marginalize_over))
+	extent = (options.getfloat(cosmo_parameters[0],"min"),options.getfloat(cosmo_parameters[0],"max"),options.getfloat(cosmo_parameters[1],"min"),options.getfloat(cosmo_parameters[1],"max"))
+
+	#Decide if showing percentages on plot
+	display_percentages = options.getboolean("contours","display_percentages")
+
+	#Marginalize over one of the parameters
+	if full_likelihood.ndim == 3:
+		marginalized_likelihood = full_likelihood.sum(marginalize_axis).transpose()
+	else:
+		marginalized_likelihood = full_likelihood.transpose()
 
 	#Normalize
 	marginalized_likelihood /= marginalized_likelihood.sum()
@@ -108,9 +159,13 @@ if __name__=="__main__":
 	#Find values and plot contours
 	fig,ax = plt.subplots()
 	
-	values,p_values = likelihood_values(marginalized_likelihood,levels=[0.684,0.90,0.99])
+	values,p_values = likelihood_values(marginalized_likelihood,levels=levels)
 	print("p_values:",p_values)
 	
-	plot_contours(ax,marginalized_likelihood,values=values,colors=["red","green","blue"])
-	fig.savefig(sys.argv[1].replace(".npy",".png")) 
+	plot_contours(ax,marginalized_likelihood,values=values,levels=levels,display_percentages=display_percentages,extent=extent,colors=colors[:len(values)])
+	
+	ax.set_xlabel(cosmo_labels[cosmo_parameters[0]])
+	ax.set_ylabel(cosmo_labels[cosmo_parameters[1]])
+	
+	fig.savefig(options.get("contours","figure_name")) 
 
