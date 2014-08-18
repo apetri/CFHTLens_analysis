5125e3751dc1bf98f1509e3295d64d1ffed10a26
apetri <apetri@phys.columbia.edu>
2014-08-18 10:29:25 -0500

    run on 112 processors

--- andrea/convergence_map_analysis/measure.sh
+++ andrea/convergence_map_analysis/measure.sh
@@ -6,11 +6,11 @@
 #SBATCH -o measureFeatures.out
 #SBATCH -e measureFeatures.err
 
-#SBATCH -n 11
+#SBATCH -n 112
 #SBATCH -p development
-#SBATCH -t 00:10:00
+#SBATCH -t 02:00:00
 
 #SBATCH --mail-user=apetri@phys.columbia.edu
 #SBATCH --mail-type=all
 
-ibrun -n 11 -o 0 python-mpi /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/measure_features.py -f /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/options_stampede.ini
+ibrun -n 112 -o 0 python-mpi /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/measure_features.py -f /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/options_stampede.ini
