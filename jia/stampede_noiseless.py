b036300caadd62f9fcb4cab19bbd7ba6a159ee97
liuxx479 <liuxx479@gmail.com>
2014-09-15 17:48:30 -0400

    process for cov test

--- jia/stampede_noiseless.py
+++ jia/stampede_noiseless.py
@@ -17,7 +17,7 @@ sim_dir = '/home1/02977/jialiu/cov_cat/'#'/home1/02977/jialiu/cat/'
 
 mask = WLanalysis.readFits(KS_dir+'mask/BAD_CFHT_mask_ngal5_sigma%02d_subfield%02d.fits'%(sigmaG*10, 1))
 #kappaGen = lambda r: WLanalysis.readFits( sim_dir+'emu1-512b240_Om0.305_Ol0.695_w-0.879_ns0.960_si0.765/emulator_subfield1_WL-only_emu1-512b240_Om0.305_Ol0.695_w-0.879_ns0.960_si0.765_4096xy_%04dr.fit'%(r)).T#[0]
-kappaGen = lambda r: WLanalysis.readFits( sim_dir+'emulator_subfield1_WL-only_cfhtcov-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.800_4096xy_%04dr.fit'%(r)).T#[0]
+kappaGen = lambda r: WLanalysis.readFits( sim_dir+'emulator_subfield1_WL-only_cfhtcov-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.800_4096xy_%04dr.fit'%(r)).T
 
 #k, s1, s2
 
