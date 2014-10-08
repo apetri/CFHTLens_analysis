17233b1162aa2ee298a9acd600d6fd68c6572f6a
apetri <apetri@phys.columbia.edu>
2014-10-08 15:41:03 -0400

    compute chi2 of best fits and save likelihood cubes

--- andrea/convergence_map_analysis/likelihood_mocks.py
+++ andrea/convergence_map_analysis/likelihood_mocks.py
@@ -60,6 +60,7 @@ def main():
 	parser.add_argument("-s","--save_points",dest="save_points",action="store",default=None,help="save points in parameter space to external npy file")
 	parser.add_argument("-ss","--save_debug",dest="save_debug",action="store_true",default=False,help="save a bunch of debugging info for the analysis")
 	parser.add_argument("-p","--prefix",dest="prefix",action="store",default="",help="prefix of the emulator to pickle")
+	parser.add_argument("-l","--likelihood",dest="likelihood",action="store_true",default=False,help="save the likelihood cubes for the mocks")
 
 	cmd_args = parser.parse_args()
 
@@ -172,6 +173,7 @@ def main():
 	first_realization = feature_loader.options.getint("mocks","first_realization")
 	last_realization = feature_loader.options.getint("mocks","last_realization")
 	best_fit_all = np.zeros((last_realization-first_realization+1,analysis.parameter_set.shape[1]))
+	chi2_all = np.zeros(last_realization-first_realization+1)
 
 	#Cycle through the realizations and obtain a best fit for each one of them
 	
@@ -186,6 +188,11 @@ def main():
 		#After chi2, compute the likelihood
 		likelihood_cube = analysis.likelihood(chi_squared.reshape(Om.shape + w.shape + si8.shape))
 
+		#Maybe save the likelihood cube?
+		if cmd_args.likelihood:
+			likelihood_filename = os.path.join(feature_loader.options.get("analysis","save_path"),"troubleshoot","likelihood{0}_{1}.npy".format(nreal+1,output_string(feature_loader.feature_string)))
+			logging.info("Saving likelihood cube to {0}...".format(best_fit_filename))
+			np.save(likelihood_filename,likelihood_cube)
 
 		#Find the maximum of the likelihood using ContourPlot functionality
 		contour = ContourPlot()
@@ -197,10 +204,12 @@ def main():
 
 		#Display the new best fit before exiting
 		best_fit_parameters = np.array([ parameters_maximum[par_key] for par_key in parameter_keys ])
-		logging.info("Best fit for realization {3} is [ {0[0]:.2f} {0[1]:.2f} {0[2]:.2f} ], chi2={1[0]:.3f}({2} dof)".format(best_fit_parameters,analysis.chi2(best_fit_parameters,features_covariance=features_covariance,observed_feature=observed_feature[nreal]),analysis.training_set.shape[1],nreal+1))
+		best_fit_chi2 = analysis.chi2(best_fit_parameters,features_covariance=features_covariance,observed_feature=observed_feature[nreal])[0]
+		logging.info("Best fit for realization {3} is [ {0[0]:.2f} {0[1]:.2f} {0[2]:.2f} ], chi2={1:.3f}({2} dof)".format(best_fit_parameters,best_fit_chi2,analysis.training_set.shape[1],nreal+1))
 
-		#Update global array with best fit parameters
-		best_fit_all[nreal,:] = best_fit_parameters.copy()
+		#Update global array with best fit parameters and corresponding chi2
+		best_fit_all[nreal-first_realization+1,:] = best_fit_parameters.copy()
+		chi2_all[nreal-first_realization+1] = best_fit_chi2 
 
 	#######################################################################################################################################################################
 	
@@ -213,6 +222,11 @@ def main():
 	best_fit_filename = os.path.join(feature_loader.options.get("analysis","save_path"),"troubleshoot","best_fit_all_{0}.npy".format(output_string(feature_loader.feature_string)))
 	logging.info("Saving best fit to {0}...".format(best_fit_filename))
 	np.save(best_fit_filename,best_fit_all)
+
+	#Save the best fit chi2 for all realizations
+	chi2_filename = os.path.join(feature_loader.options.get("analysis","save_path"),"troubleshoot","chi2_all_{0}.npy".format(output_string(feature_loader.feature_string)))
+	logging.info("Saving best fit chi2 to {0}...".format(chi2_filename))
+	np.save(chi2_filename,chi2_all)
 
 	end = time.time()
 
