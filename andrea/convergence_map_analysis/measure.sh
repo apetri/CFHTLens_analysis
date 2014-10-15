30a066139bea93a0f8e429c712c545dc03889740
apetri <apetri@phys.columbia.edu>
2014-10-15 14:39:14 -0500

    python --> python-mpi

--- andrea/convergence_map_analysis/measure.sh
+++ andrea/convergence_map_analysis/measure.sh
@@ -13,4 +13,4 @@
 #SBATCH --mail-user=apetri@phys.columbia.edu
 #SBATCH --mail-type=all
 
-ibrun -n 112 -o 0 python /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/measure_features.py -f /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/options_stampede.ini
+ibrun -n 112 -o 0 /opt/apps/intel14/mvapich2_2_0/python/2.7.6/lib/python2.7/site-packages/mpi4py/bin/python-mpi /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/measure_features.py -f /home1/02918/apetri/CFHTLens_analysis/andrea/convergence_map_analysis/options_stampede.ini
