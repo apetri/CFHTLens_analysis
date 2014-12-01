1e612c81b3006e7bd971b04959d6449e8ca4d5cb
liuxx479 <liuxx479@gmail.com>
2014-12-01 18:12:09 -0500

    full covariance bug fix

--- jia/test_chisq_cube_MPI.py
+++ jia/test_chisq_cube_MPI.py
@@ -125,10 +125,15 @@ def plot_heat_map_w (values):
 ############ operation ####################################
 ##########################################################
 pool=MPIPool()
+print 'boo'
 idx = idx_arr[nn]
+
 interp_cosmo, cov_mat, cov_inv, ps_CFHT = return_interp_cosmo_for_idx (idx)
+print 'boo2'
 values = [[w, idx, interp_cosmo, cov_inv, ps_CFHT] for w in w_arr]
+
 pool.map(plot_heat_map_w, values)
+
 cube = array([load('/home1/02977/jialiu/chisq_cube/test/chisqcube_%s_w%s.npy'%(fn_arr[nn], w)) for w in w_arr])
 
 save(test_dir+'covmat_%s.npy'%(fn_arr[nn]), cov_mat)
