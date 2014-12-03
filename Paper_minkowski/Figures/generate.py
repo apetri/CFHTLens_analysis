7f148faedd5b33d91eb86e4bdc55a2ad84a0b355
apetri <apetri@phys.columbia.edu>
2014-12-03 17:42:27 -0500

    generate more figures

--- Paper_minkowski/Figures/generate.py
+++ Paper_minkowski/Figures/generate.py
@@ -10,8 +10,11 @@ from contours import ContourPlot
 
 axes_facecolor = rc.func_globals["rcParams"]["axes.facecolor"]
 
+#Locations
 root_dir = "/Users/andreapetri/Documents/Columbia/CFHTLens_analysis/andrea/convergence_map_analysis/cfht_masked_BAD_clipped"
 design_points = "/Users/andreapetri/Documents/Cosmology_software/LensTools/lenstools/data/CFHTemu1_array.npy"
+
+#Colors
 brew_colors = ["red","green","blue","black","orange","magenta","cyan"]
 brew_colors_11 = ["#a50026","#d73027","#f46d43","#fdae61","#fee08b","#ffffbf","#d9ef8b","#a6d96a","#66bd63","#1a9850","#006837"]
 brew_colors_diverging = ["#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928"]
@@ -27,13 +30,43 @@ descriptors["moments"]=r"$\mathrm{Moments}$"
 #Number of principal components
 num_components = dict()
 num_components["power_spectrum"] = 3
-num_components["minkowski_0"] = 3
-num_components["minkowski_1"] = 3
-num_components["minkowski_2"] = 3
-num_components["moments"] = 3  
+num_components["minkowski_0"] = 5
+num_components["minkowski_1"] = 20
+num_components["minkowski_2"] = 20
+num_components["moments"] = 9 
+
+#Smoothing scales
+smoothing_scales = dict()
+smoothing_scales["power_spectrum"] = 1.0
+smoothing_scales["minkowski_0"] = 1.0
+smoothing_scales["minkowski_1"] = 1.0
+smoothing_scales["minkowski_2"] = 1.0 
+smoothing_scales["moments"] = 1.0
 
 keys = descriptors.keys()
 keys.sort()
+
+################################################################################################################################################
+################################################################################################################################################
+
+def _cross_name(*args):
+
+	names = list()
+	for arg in args:
+		names.append("{0}--{1:.1f}_ncomp{2}".format(arg,smoothing_scales[arg],num_components[arg]))
+
+	return "-".join(names) 
+
+def _cross_label(*args):
+
+	labels = list()
+	for arg in args:
+		labels.append(descriptors[arg]+r"$({0})$".format(num_components[arg]))
+
+	return r" $\times$ ".join(labels)
+
+
+################################################################################################################################################
 
 def design(cmd_args):
 
@@ -237,25 +270,20 @@ def robustness(cmd_args):
 
 ##################################################################################################################################################
 
-def comparison(cmd_args):
+def contours_combine(cmd_args,mock=False):
+
+	#These are the statistics to cross
+	single = ["power_spectrum","minkowski_0"]
+	multiple = [("minkowski_0","minkowski_1","minkowski_2"),("power_spectrum","minkowski_0","minkowski_1","minkowski_2"),("power_spectrum","minkowski_0","minkowski_1","minkowski_2","moments")]
+
+	#decide if consider data or simulations
+	if mock:
+		mock_prefix="mock"
+	else:
+		mock_prefix=""
 
 	#Smoothing scales in arcmin
-	smoothing_scale=1.0
-	n_components=30
-
-	#Likelihood levels
 	levels = [0.684]
-
-	#Descriptors
-	descriptors_comparison = ["minkowski_0--{0:.1f}","minkowski_1--{0:.1f}","minkowski_2--{0:.1f}","minkowski_012--{0:.1f}_power_spectrum--{0:.1f}","minkowski_0--{0:.1f}_minkowski_1--{0:.1f}_minkowski_2--{0:.1f}","power_spectrum--{0:.1f}"]
-	descriptor_titles = dict()
-	descriptor_titles["minkowski_0--{0:.1f}"] = r"$V_0$"
-	descriptor_titles["minkowski_1--{0:.1f}"] = r"$V_1$"
-	descriptor_titles["minkowski_2--{0:.1f}"] = r"$V_2$"
-	descriptor_titles["minkowski_0--{0:.1f}_minkowski_1--{0:.1f}_minkowski_2--{0:.1f}"] = r"$V_0 \times V_1 \times V_2$"
-	descriptor_titles["power_spectrum--{0:.1f}"] = r"$\mathrm{PS}$"
-	descriptor_titles["moments--{0:.1f}"] = r"$\mathrm{Moments}$"
-	descriptor_titles["minkowski_012--{0:.1f}_power_spectrum--{0:.1f}"] = r"$V_0 \times V_1 \times V_2 \times$ $\mathrm{PS}$"
 
 	#Parameters of which we want to compute the confidence estimates
 	parameter_axes = {"Omega_m":0,"w":1,"sigma8":2}
@@ -266,43 +294,73 @@ def comparison(cmd_args):
 	with open(cmd_args.options_file,"r") as configfile:
 		options.readfp(configfile)
 
-	#Create the figure
+	#Create figure
 	fig,ax = plt.subplots()
 
-	#Cycle over the desctiptors
-	for n,descr in enumerate(descriptors_comparison):
+	#Plot labels
+	contour_labels = list()
+
+	#Cycle over descriptors
+	for n,descr in enumerate(single+multiple):
 
 		#Instantiate contour plot
 		contour = ContourPlot(fig=fig,ax=ax)
 
-		#Load the likelihood
-		likelihood_file = os.path.join(root_dir,"likelihoods","likelihoodmock_"+descr.format(smoothing_scale)+"_ncomp{0}.npy".format(n_components))
-		contour.getLikelihood(likelihood_file,parameter_axes=parameter_axes,parameter_labels=cosmo_labels)
+		#Construct the likelihood file
+		if type(descr)==str:
+			likelihood_file = os.path.join(root_dir,"likelihoods","likelihood{0}_{1}--{2:.1f}_ncomp{3}.npy".format(mock_prefix,descr,smoothing_scales[descr],num_components[descr]))
+			contour_labels.append(descriptors[descr]+r"$({0})$".format(num_components[descr]))
+		elif type(descr)==tuple:
+			likelihood_file = os.path.join(root_dir,"likelihoods","likelihood{0}_cross_{1}.npy".format(mock_prefix,_cross_name(*descr)))
+			contour_labels.append(_cross_label(*descr))
+		else:
+			raise TypeError("type not valid")
 
-		#Set physical units
+		#Log filename
+		print("Loading likelihood from {0}".format(likelihood_file))
+		
+		#Load the likelihood
+		contour.getLikelihood(likelihood_file,parameter_axes=parameter_axes,parameter_labels=cosmo_labels)
+		
+		#Set the physical units
 		contour.getUnitsFromOptions(options)
 
 		#Marginalize
 		contour.marginalize("w")
+		
+		#Slice on best fit for w
+		maximum = contour.getMaximum()
+		print("Likelihood with {0} is maximum at {1}".format(descr,maximum))
 
 		#Get levels
 		contour.getLikelihoodValues(levels=levels)
 
-		#Plot the contour
-		contour.plotContours(colors=[brew_colors[n]],fill=False,display_percentages=False,display_maximum=False)
+		#Plot contours
+		contour.plotContours(colors=[brew_colors[n]],fill=False,display_maximum=False,display_percentages=False,alpha=1.0)
 
-	#Labels
+	
+	#Legend
 	contour.title_label=""
-	contour.labels(contour_label=[descriptor_titles[descr] for descr in descriptors_comparison])
+	contour.labels(contour_labels)
 
-	#Save the figure
-	fig.savefig("comparison_pca{0}.{1}".format(n_components,cmd_args.type))
-
-
+	#Save
+	fig.savefig("contours{0}_cross.{1}".format(mock_prefix,cmd_args.type))	
 
 ##################################################################################################################################################
 
-def w_likelihood(cmd_args):
+def contours_combine_mock(cmd_args):
+
+	contours_combine(cmd_args,mock=True)
+
+##################################################################################################################################################
+
+def w_likelihood(cmd_args,mock=False):
+
+	#decide if consider data or simulations
+	if mock:
+		mock_prefix="mock"
+	else:
+		mock_prefix=""
 
 	#Parameters of which we want to compute the confidence estimates
 	parameter_axes = {"Omega_m":0,"w":1,"sigma8":2}
@@ -315,9 +373,6 @@ def w_likelihood(cmd_args):
 
 	#Smoothing scales in arcmin
 	smoothing_scale=1.0
-
-	#Number of components
-	n_components=3
 
 	#Create figure
 	fig,ax = plt.subplots()
@@ -326,7 +381,7 @@ def w_likelihood(cmd_args):
 	for n,descr in enumerate(keys):
 
 		contour = ContourPlot()
-		likelihood_file = os.path.join(root_dir,"likelihoods","likelihood_{0}--{1:.1f}_ncomp{2}.npy".format(descr,smoothing_scale,n_components))
+		likelihood_file = os.path.join(root_dir,"likelihoods","likelihood{0}_{1}--{2:.1f}_ncomp{3}.npy".format(mock_prefix,descr,smoothing_scale,num_components[descr]))
 
 		contour.getLikelihood(likelihood_file,parameter_axes=parameter_axes,parameter_labels=cosmo_labels)
 
@@ -335,7 +390,7 @@ def w_likelihood(cmd_args):
 
 		#Compute marginal likelihood over w
 		w,l = contour.marginal("w")
-		ax.plot(w,l,label=descriptors[descr],color=brew_colors[n])
+		ax.plot(w,l,label=descriptors[descr]+r"$({0})$".format(num_components[descr]),color=brew_colors[n])
 
 	#Legend
 	ax.set_xlabel(r"$w$",fontsize=18)
@@ -343,7 +398,89 @@ def w_likelihood(cmd_args):
 	ax.legend()
 
 	#Save
-	fig.savefig("w_likelihood.{0}".format(cmd_args.type))
+	fig.savefig("w{0}_likelihood.{1}".format(mock_prefix,cmd_args.type))
+
+###################################################################################################################################################
+
+def w_mock_likelihood(cmd_args):
+
+	w_likelihood(cmd_args,mock=True)
+
+
+###################################################################################################################################################
+
+def w_likelihood_combine(cmd_args,mock=False):
+
+	#These are the statistics to cross
+	single = ["power_spectrum","minkowski_0"]
+	multiple = [("minkowski_0","minkowski_1","minkowski_2"),("power_spectrum","minkowski_0","minkowski_1","minkowski_2"),("power_spectrum","minkowski_0","minkowski_1","minkowski_2","moments")]
+
+	#decide if consider data or simulations
+	if mock:
+		mock_prefix="mock"
+	else:
+		mock_prefix=""
+
+	#Smoothing scales in arcmin
+	levels = [0.684]
+
+	#Parameters of which we want to compute the confidence estimates
+	parameter_axes = {"Omega_m":0,"w":1,"sigma8":2}
+	cosmo_labels = {"Omega_m":r"$\Omega_m$","w":r"$w$","sigma8":r"$\sigma_8$"}
+
+	#Parse options from configuration file
+	options = ConfigParser.ConfigParser()
+	with open(cmd_args.options_file,"r") as configfile:
+		options.readfp(configfile)
+
+	#Create figure
+	fig,ax = plt.subplots()
+
+	#Plot labels
+	contour_labels = list()
+
+	#Cycle over descriptors
+	for n,descr in enumerate(single+multiple):
+
+		#Instantiate contour plot
+		contour = ContourPlot(fig=fig,ax=ax)
+
+		#Construct the likelihood file
+		if type(descr)==str:
+			likelihood_file = os.path.join(root_dir,"likelihoods","likelihood{0}_{1}--{2:.1f}_ncomp{3}.npy".format(mock_prefix,descr,smoothing_scales[descr],num_components[descr]))
+			contour_labels.append(descriptors[descr]+r"$({0})$".format(num_components[descr]))
+		elif type(descr)==tuple:
+			likelihood_file = os.path.join(root_dir,"likelihoods","likelihood{0}_cross_{1}.npy".format(mock_prefix,_cross_name(*descr)))
+			contour_labels.append(_cross_label(*descr))
+		else:
+			raise TypeError("type not valid")
+
+		#Log filename
+		print("Loading likelihood from {0}".format(likelihood_file))
+		
+		#Load the likelihood
+		contour.getLikelihood(likelihood_file,parameter_axes=parameter_axes,parameter_labels=cosmo_labels)
+		
+		#Set the physical units
+		contour.getUnitsFromOptions(options)
+
+		#Calculate the w marginal likelihood
+		w,l = contour.marginal("w")
+		ax.plot(w,l,label=contour_labels[-1])
+	
+	#Legend
+	ax.set_xlabel(r"$w$",fontsize=18)
+	ax.set_ylabel(r"$\mathcal{L}(w)$",fontsize=18)
+	ax.legend(loc="upper left",prop={"size":10})
+
+	#Save
+	fig.savefig("w{0}_likelihood_cross.{1}".format(mock_prefix,cmd_args.type))
+
+###################################################################################################################################################
+
+def w_mock_likelihood_combine(cmd_args):
+
+	w_likelihood_combine(cmd_args,mock=True)
 
 ############################################################################################################################
 ############################################################################################################################
@@ -352,7 +489,13 @@ figure_method = dict()
 figure_method["2"] = design
 figure_method["3"] = pca
 figure_method["4"] = robustness
+figure_method["4b"] = w_mock_likelihood
 figure_method["5"] = contours
+figure_method["6"] = w_likelihood
+figure_method["7"] = contours_combine
+figure_method["7b"] = contours_combine_mock
+figure_method["8"] = w_likelihood_combine
+figure_method["8b"] = w_mock_likelihood_combine
 
 
 if __name__=="__main__":
