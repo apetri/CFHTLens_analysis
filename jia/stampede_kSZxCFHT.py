caf28842bafb0a431cb31fa8e56e3ab51267ed0c
liuxx479 <liuxx479@gmail.com>
2015-01-21 17:30:08 -0500

    add kSZxCFHT to stampede

--- jia/stampede_kSZxCFHT.py
+++ jia/stampede_kSZxCFHT.py
@@ -24,7 +24,7 @@ create_noise_KS = 1
 ############### create noise kappa maps #############
 #####################################################
 if create_noise_KS:
-	Wx=int(sys.argv(1))
+	Wx=int(sys.argv[1])
 	p = MPIPool()
 	Mexw = lambda Wx, txt: WLanalysis.readFits(kSZ_dir+'CFHT/Me_Mw_galn/W%i_M%s_1.3_lo.fit'%(Wx,txt))
 	Me1, Me2, Mwm = Mexw(Wx, 'e1w'), Mexw(Wx, 'e2w'), Mexw(Wx, 'wm')
