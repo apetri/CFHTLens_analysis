d4e4d937b1763927f488f1e38d039ca21640357e
liuxx479 <liuxx479@gmail.com>
2015-03-03 12:28:57 -0500

    regular update

--- jia/batch_filegen.py
+++ jia/batch_filegen.py
@@ -3,17 +3,17 @@ N = 1
 n = N*16
 def write_file(errfn, program_name, variable):
 	f = open('/home1/02977/jialiu/batch/batch_{0}_{1}'.format(errfn, variable), 'w')
-	content = 	'''#!/bin/bash
-	#SBATCH -J {0}{2}    # Job name
-	#SBATCH -o {0}{2}.e%j # Name of stdout output file
-	#SBATCH -e {0}{2}.e%j # Name of stderr output file
-	#SBATCH -p normal       # Submit to the 'normal' or 'development' queue
-	#SBATCH -N {3}           # Total number of nodes requested (16 cores/node)
-	#SBATCH -n {4}           # Total number of mpi tasks requested
-	#SBATCH -t 0:30:00      # Run time (hh:mm:ss)
-	#SBATCH -A TG-AST140041
+	content = '''#!/bin/bash
+#SBATCH -J {0}{2}    # Job name
+#SBATCH -o {0}{2}.e%j # Name of stdout output file
+#SBATCH -e {0}{2}.e%j # Name of stderr output file
+#SBATCH -p normal       # Submit to the 'normal' or 'development' queue
+#SBATCH -N {3}           # Total number of nodes requested (16 cores/node)
+#SBATCH -n {4}           # Total number of mpi tasks requested
+#SBATCH -t 0:30:00      # Run time (hh:mm:ss)
+#SBATCH -A TG-AST140041
 
-	ibrun python /home1/02977/jialiu/CFHTLens_analysis/jia/{1} {2}'''.format(errfn, program_name, variable, N, n)
+ibrun python /home1/02977/jialiu/CFHTLens_analysis/jia/{1} {2}'''.format(errfn, program_name, variable, N, n)
 	f.write(content)
 	f.close()
 
