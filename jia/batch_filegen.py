326e8f72758daa0beb14163037ada0165a29e3bd
liuxx479 <liuxx479@gmail.com>
2015-04-03 01:00:47 -0400

    regular update

--- jia/batch_filegen.py
+++ jia/batch_filegen.py
@@ -17,7 +17,7 @@ def write_file(errfn, program_name, variable):
 #SBATCH -p normal       # Submit to the 'normal' or 'development' queue
 #SBATCH -N {3}           # Total number of nodes requested (16 cores/node)
 #SBATCH -n {4}          # Total number of mpi tasks requested
-#SBATCH -t 0:30:00      # Run time (hh:mm:ss)
+#SBATCH -t 3:00:00      # Run time (hh:mm:ss)
 #SBATCH -A TG-AST140041
 
 ibrun python /home1/02977/jialiu/CFHTLens_analysis/jia/{1} {2}'''.format(errfn, program_name, variable, N, n)
