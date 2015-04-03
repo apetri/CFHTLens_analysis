c2a6886a3c53e156dd6658bb875f81022dbd2e1b
liuxx479 <liuxx479@gmail.com>
2015-04-03 00:07:21 -0400

    regular update

--- jia/batch_filegen.py
+++ jia/batch_filegen.py
@@ -1,5 +1,12 @@
 #!python
-N = 1
+############# edite this block ###########
+############# maybe also want to edit run time below #####
+errfn = 'massSIM' # error file name, will append variable automatically
+program_name = 'stampede_massSIM.py' #program name
+N = 4 # number of nodes
+
+##################
+
 n = N*16
 def write_file(errfn, program_name, variable):
 	f = open('/home1/02977/jialiu/batch/batch_{0}_{1}'.format(errfn, variable), 'w')
@@ -10,14 +17,12 @@ def write_file(errfn, program_name, variable):
 #SBATCH -p normal       # Submit to the 'normal' or 'development' queue
 #SBATCH -N {3}           # Total number of nodes requested (16 cores/node)
 #SBATCH -n {4}           # Total number of mpi tasks requested
-#SBATCH -t 0:30:00      # Run time (hh:mm:ss)
+#SBATCH -t 2:00:00      # Run time (hh:mm:ss)
 #SBATCH -A TG-AST140041
 
 ibrun python /home1/02977/jialiu/CFHTLens_analysis/jia/{1} {2}'''.format(errfn, program_name, variable, N, n)
 	f.write(content)
 	f.close()
 
-errfn = 'noiselessPDF'
-program_name = 'stampede_noiseless_PDF.py'
 for variable in range(1,14):
 	write_file(errfn, program_name, variable)
