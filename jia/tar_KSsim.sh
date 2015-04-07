6a91c5354239a13d17aa181ab2b30f5c5454d4b9
liuxx479 <liuxx479@gmail.com>
2015-04-07 12:53:25 -0400

    regular update

--- jia/tar_KSsim.sh
+++ jia/tar_KSsim.sh
@@ -4,12 +4,12 @@ f=/work/02977/jialiu/KSsim
 cd $f
 for i in  `ls $f`
 do echo $i
-tar -cvf $i.tar $i
+tar -cf /scratch/02977/jialiu/ranch_archive/KSsim/$i.tar $i
 done
 
 f=/work/02977/jialiu/KSsim_noiseless
 cd $f
 for i in  `ls $f`
 do echo $i
-tar -cvf $i.tar $i
+tar -cf /scratch/02977/jialiu/ranch_archive/KSsim_noiseless/$i.tar $i
 done
