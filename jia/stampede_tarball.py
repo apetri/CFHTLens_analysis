90bea37f3d8c64035a4f630fcc81587b64169203
liuxx479 <liuxx479@gmail.com>
2015-04-26 13:23:12 -0400

    regular update

--- jia/stampede_tarball.py
+++ jia/stampede_tarball.py
@@ -1,19 +1,21 @@
 import os
-import subprocess
+#import subprocess
 from emcee.utils import MPIPool
 
 inDIR_arr = ['/scratch/02977/jialiu/KSsim_noiseless/', '/scratch/02977/jialiu/KSsim/']
 
 outDIR_arr = ['/scratch/02977/jialiu/ranch_archive/KSsim_noiseless/', '/scratch/02977/jialiu/ranch_archive/KSsim/']
 
-#for outDIR in outDIR_arr:
+for outDIR in outDIR_arr:
 	#subprocess.check_call('lfs setstripe -c 4 %s'%(outDIR))
+	os.system("lfs setstripe -c 4 %s"%(outDIR))
 
 
 
 def create_tarball (FNs):
 	inFN, outFN = FNs
-	subprocess.check_call('tar cfv %s.tar %s'%(outFN, inFN))
+	#subprocess.check_call("tar cfv %s.tar %s"%(outFN, inFN))
+	os.system("tar cfv %s.tar %s"%(outFN, inFN))
 
 pool = MPIPool()
 for i in range(len(inDIR_arr)):
