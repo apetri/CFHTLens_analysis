689d5c47493414777ee284362453809923abc001
liuxx479 <liuxx479@gmail.com>
2015-06-17 18:00:44 +0200

    regular update

--- jia/batch_gadget_submission.py
+++ jia/batch_gadget_submission.py
@@ -1,6 +1,6 @@
 #!python
 def write_file(ic):
-	f = open('/work/02977/jialiu/lenstools_home/Jobs/gadget_ic{0}.sh'.format(ic), 'w')
+	f = open('/work/02977/jialiu/lenstools_home/Jobs/gadget_3ic{0}.sh'.format(ic), 'w')
 	content = '''#!/bin/bash
 
 ################################
@@ -16,12 +16,12 @@ def write_file(ic):
 
 #SBATCH -J Gadget2_ic{0}
 
-#SBATCH -o /work/02977/jialiu/lenstools_home/Logs/gadget_ic{0}_%j.err
-#SBATCH -e /work/02977/jialiu/lenstools_home/Logs/gadget_ic{0}_%j.err
+#SBATCH -o /work/02977/jialiu/lenstools_home/Logs/gadget_3ic{0}_%j.err
+#SBATCH -e /work/02977/jialiu/lenstools_home/Logs/gadget_3ic{0}_%j.err
 
 
 #SBATCH -p normal
-#SBATCH -t 02:00:00
+#SBATCH -t 05:00:00
 
 #SBATCH --mail-user=jia@astro.columbia.edu
 #SBATCH --mail-type=all
@@ -31,16 +31,19 @@ def write_file(ic):
 #############Resources####################
 ##########################################
 
-#SBATCH -n 256
-#SBATCH -N 16
+#SBATCH -n 768
+#SBATCH -N 48
 
 ###################################################
 #################Execution#########################
 ###################################################
 
-ibrun -n 256 -o 0 /work/02977/jialiu/IG_Pipeline_0.1/Gadget2/Gadget2 1 256 /work/02977/jialiu/lenstools_home/Om0.300_Ol0.700/512b240/ic{0}/gadget2.param'''.format(ic)
+ibrun -n 256 -o 0 /work/02977/jialiu/IG_Pipeline_0.1/Gadget2/Gadget2 1 256 /work/02977/jialiu/lenstools_home/Om0.300_Ol0.700/512b240/ic{0}/gadget2.param &
+ibrun -n 256 -o 256 /work/02977/jialiu/IG_Pipeline_0.1/Gadget2/Gadget2 1 256 /work/02977/jialiu/lenstools_home/Om0.300_Ol0.700/512b240/ic{1}/gadget2.param &
+ibrun -n 256 -o 512 /work/02977/jialiu/IG_Pipeline_0.1/Gadget2/Gadget2 1 256 /work/02977/jialiu/lenstools_home/Om0.300_Ol0.700/512b240/ic{2}/gadget2.param &
+wait'''.format(ic, ic+1, ic+2)
 	f.write(content)
 	f.close()
 
 
-map(write_file, [2]+range(334,430))
+map(write_file, range(334,430)[::3])
