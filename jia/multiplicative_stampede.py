8719de952d7eb2d75cff99ec2eb3f12eb7cb66c7
liuxx479 <liuxx479@gmail.com>
2015-10-16 11:50:01 -0400

    regular update

--- jia/multiplicative_stampede.py
+++ jia/multiplicative_stampede.py
@@ -135,10 +135,11 @@ class GRF_Gen:
 ########### compute sim error ######################
 seednum=0
 #for Wx in range(1,5):
-Ckmap = CkappaGen(Wx)
-Pkmap = PkappaGen(Wx)
 	#for cut in (22, 23, 24):
 Wx, cut = int(sys.argv[1]), int(sys.argv[2])
+Ckmap = CkappaGen(Wx)
+Pkmap = PkappaGen(Wx)
+
 print 'Wx, cut', Wx, cut
 galn = galnGen(Wx, cut)
 
