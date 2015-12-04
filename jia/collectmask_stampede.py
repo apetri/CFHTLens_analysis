7b4067839f331b2f3426dce7c2ad26ad58655da8
liuxx479 <liuxx479@gmail.com>
2015-12-04 18:46:43 -0500

    regular update

--- jia/collectmask_stampede.py
+++ jia/collectmask_stampede.py
@@ -32,29 +32,33 @@ center=centers[Wx-1]
 headers=hdulist[0].header
 w=wcs.WCS(headers)
 footprint = w.calc_footprint()
-data = np.array(hdulist[0].data)
-data[data<=1]=0.0
-data[data>1]=1.0
-#print 'got data',time.strftime("%Y-%m-%d %H:%M")
-xy=np.indices(data.shape)
+
+############ ONCE TIME: saves part of the data into files ######
+#data = np.array(hdulist[0].data)
+#data[data<=1]=0.0
+#data[data>1]=1.0#mask out everything has mask<1
+#step = ceil(data.shape[1]/63.0)
+#for icount in range(63):
+    #print icount
+    #idata=data[:,step*icount:step*(1+icount)]#.flatten().reshape(1,-1)
+    #save(mask_dir+'smaller/cat_W%i_step%i_start%i'%(Wx,step, icount), idata)
+#################################################################
+
+#import time
+#print 'begin',time.strftime("%Y-%m-%d %H:%M")
 f_Wx = WLanalysis.gnom_fun(centers[Wx-1])
-step = ceil(data.shape[1]/63.0)
 
-import time
-print 'begin',time.strftime("%Y-%m-%d %H:%M")
-
-# 1 field needs to be split into 24 cores to do
+# 1 field needs to be split into 63 cores to do
 def partialdata2grid (icount):
     '''for a small portion of the data, put into a grid'''
     print 'icount',icount
-    ixy = xy[:,:,step*icount:step*(1+icount)]
-    radeclist = (array(w.wcs_pix2world(ixy[0],ixy[1],0)).T).reshape(-1,2) 
-    
-    ####### recycle xy, to save space..
+    idata = load(mask_dir+'smaller/cat_W%i_step%i_start%i.npy'%(Wx,step, icount))
+    ix, iy=np.indices(idata.shape)
+    iy+=step*icount
+    radeclist = (array(w.wcs_pix2world(ix, iy, 0)).T).reshape(-1,2) 
+
     y, x = f_Wx (radeclist)
-    idata=data[:,step*icount:step*(1+icount)].flatten().reshape(1,-1)
-    #y, x = xy.T
-    ipix, ipix_mask = WLanalysis.coords2grid(x, y, idata, size=sizes[Wx-1])
+    ipix, ipix_mask = WLanalysis.coords2grid(x, y, idata.flatten().reshape(1,-1), size=sizes[Wx-1])
     print icount,'done coords2grid',time.strftime("%Y-%m-%d %H:%M")
     
     save(mask_dir+'smaller/W%i_%i_numpix'%(Wx,icount), ipix)
