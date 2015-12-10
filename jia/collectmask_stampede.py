79a35c78ddecdeb7d6fda62039de0602afe9517c
liuxx479 <liuxx479@gmail.com>
2015-12-09 22:21:37 -0500

    regular update

--- jia/collectmask_stampede.py
+++ jia/collectmask_stampede.py
@@ -35,17 +35,17 @@ footprint = w.calc_footprint()
 step = [522, 294, 420, 351][Wx-1]# ceil(data.shape[1]/63.0)
 
 ############ ONCE TIME: saves part of the data into files ######
-#data = np.array(hdulist[0].data)
-#data[data<=1]=0.0
-#data[data>1]=1.0#mask out everything has mask<1
+data = np.array(hdulist[0].data)
+data[data<=1]=0.0
+data[data>1]=1.0#mask out everything has mask<1
 
-#for icount in range(63):
-    #print icount
-    #idata=data[:,step*icount:step*(1+icount)]#.flatten().reshape(1,-1)
-    #save(mask_dir+'smaller/cat_W%i_step%i_start%i'%(Wx,step, icount), idata)
+for icount in range(63):
+    print icount
+    idata=data[:,step*icount:step*(1+icount)]#.flatten().reshape(1,-1)
+    save(mask_dir+'smaller/cat_W%i_step%i_start%i'%(Wx,step, icount), idata)
 #################################################################
 
-import time
+#import time
 #print 'begin',time.strftime("%Y-%m-%d %H:%M")
 f_Wx = WLanalysis.gnom_fun(centers[Wx-1])
 
@@ -54,15 +54,29 @@ def partialdata2grid (icount):
     '''for a small portion of the data, put into a grid'''
     print 'icount',icount
     idata = load(mask_dir+'smaller/cat_W%i_step%i_start%i.npy'%(Wx,step, icount))
+    print 'loaded',icount
     ix, iy=np.indices(idata.shape)
     iy+=step*icount
     #radeclist = (array(w.wcs_pix2world(ix, iy, 0)).reshape(2,-1)).T ////jia changed on 12/9, since the coordinates seems to be off..
     #radeclist = (array(w.wcs_pix2world(iy, ix, 0)).reshape(2,-1)).T 
     #y, x = f_Wx (radeclist)
-    iy, ix = f_Wx ((array(w.wcs_pix2world(iy, ix, 0)).reshape(2,-1)).T )
-    print icount,'done f_wx %s'%(icount),time.strftime("%Y-%m-%d %H:%M")    
-    ipix_mask,ipix = WLanalysis.coords2grid(ix, iy, idata.flatten().reshape(1,-1), size=sizes[Wx-1])
-    print icount,'done coords2grid %s'%(icount),time.strftime("%Y-%m-%d %H:%M")    
+    y, x = f_Wx ((array(w.wcs_pix2world(iy, ix, 0)).reshape(2,-1)).T )
+    print icount,'done f_wx %s'%(icount)#,time.strftime("%Y-%m-%d %H:%M")
+    
+    istep=ceil(len(y)/10.0)
+    
+    ipix_mask = zeros(shape=(sizes[Wx-1], sizes[Wx-1]))
+    ipix = ipx_max.copy()
+    jj=0
+    
+    while jj< len(y):
+        print 'icount, jj',icount,jj
+        iipix_mask,iipix = WLanalysis.coords2grid(ix[jj:jj+istep], iy[jj:jj+istep], idata.flatten().reshape(1,-1)[jj:jj+istep], size=sizes[Wx-1])
+        ipix_mask += iipix_mask
+        ipix += iipix
+        jj+=istep
+    #print icount,'done coords2grid %s'%(icount),time.strftime("%Y-%m-%d %H:%M")    
+    
     save(mask_dir+'smaller/W%i_%i_numpix'%(Wx,icount), ipix)
     save(mask_dir+'smaller/W%i_%i_nummask'%(Wx,icount), ipix_mask)
     #ipix is the num. of pixels fall in that big pix, ipix_mask is the mask
@@ -73,7 +87,7 @@ if not p.is_master():
     p.wait()
     sys.exit(0)
 
-ismall_map=p.map(partialdata2grid, range(63))
+ismall_map=p.map(partialdata2grid, range(127))
 small_map = sum(array(ismall_map),axis=0)
 save(mask_dir+'W%i_smaller_mask.npy'%(Wx),small_map)
 
