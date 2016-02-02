85e13c11a08d95613e1c4ed97e00f50669980582
liuxx479 <liuxx479@gmail.com>
2016-02-02 15:45:16 -0500

    regular update

--- jia/stampede_tarball.py
+++ jia/stampede_tarball.py
@@ -1,10 +1,6 @@
 import os
-#import subprocess
 from emcee.utils import MPIPool
 
-#inDIR_arr = ['/scratch/02977/jialiu/KSsim_noiseless/', '/scratch/02977/jialiu/KSsim/']
-
-#outDIR_arr = ['/scratch/02977/jialiu/ranch_archive/KSsim_noiseless/', '/scratch/02977/jialiu/ranch_archive/KSsim/']
 
 inDIR = '/scratch/02977/jialiu/CMB_hopper/CMB_batch_storage/'
 outDIR = '/scratch/02977/jialiu/ranch_archive/CMB_batch_storage/'
@@ -16,7 +12,6 @@ def create_tarball (FNs):
     os.system("tar cfv %s.tar %s"%(outFN, inFN))
 
 pool = MPIPool()
-#for i in (1,):#range(len(inDIR_arr)):
 cosmo_arr = os.listdir(inDIR)
 FNs_arr = [['%s%s'%(inDIR, cosmo_arr[j]),'%s%s'%(outDIR, cosmo_arr[j])] for j in range(len(cosmo_arr))]
 pool.map(create_tarball, FNs_arr)
